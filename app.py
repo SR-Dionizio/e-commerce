ac8a15c47b5af2d6dd86e4e804e5db3e2ad2309a
Daniel_pc_casa <daniel_santos300@hotmail.com>
2024-08-31 13:56:34 -0300

    feat: criando a adição de produtos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 # Iniciando a aplicação e o banco
@@ -14,9 +14,20 @@ class Product(db.Model):
     description = db.Column(db.Text, nullable=True)
 
 
-@app.route('/')
-def hello_word():
-    return 'Hello word'
+@app.route('/api/products/add', methods=["POST"])
+def add_product():
+    data = request.get_json()
+    if 'name' in data and 'price' in data:
+        product = Product(
+            name=data["name"],
+            price=data["price"],
+            description=data.get("description", "")
+        )
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"}), 200
+    return jsonify({"message": "invalid product data"}), 400
+
 
 
 if __name__ == "__main__":
